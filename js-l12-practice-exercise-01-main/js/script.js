779d76273486584cf86c1a6896648de7dd970d35
Tiffin Filion <tfilion@rpa.com>
2023-04-07 16:29:44 -0700

    add ability to select multiple random users

--- js-l12-practice-exercise-01-main/js/script.js
+++ js-l12-practice-exercise-01-main/js/script.js
@@ -1,1 +1,30 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users"); 
+
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
+    const data = await usersRequest.json();
+    const userResults = data.results;
+    displayUsers(userResults);
+}
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    for (let user of userResults) {
+        let country = user.location.country;
+        let name = user.name.first;
+        let imageUrl = user.picture.medium;
+
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `<h3>${name}</h3><p>${country}</p><img src=${imageUrl} alt="User avatar" />`;
+
+        randomFolks.append(userDiv);
+    }
+}
+
+selectUserNumber.addEventListener("change", function(e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+})
+
+getData(1);
