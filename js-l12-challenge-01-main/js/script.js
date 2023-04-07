f72e53afbf2e962a4e403a2a7aff1fb6871e7518
Tiffin Filion <tfilion@rpa.com>
2023-04-07 15:35:57 -0700

    get random image and display with button click

--- js-l12-challenge-01-main/js/script.js
+++ js-l12-challenge-01-main/js/script.js
@@ -6,7 +6,23 @@ const img = document.querySelector(".img");
 const getImage = async function() {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getImage();
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+}
+
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.setAttribute("src", imageAddress);
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function(e) {
+    getImage();
+})
