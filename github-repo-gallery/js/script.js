cc13d2ee1574ff3d97122f310840f1f993bebedc
Tiffin Filion <tiffin.filion@gmail.com>
2023-04-09 13:12:40 -0700

    add individual repo info

--- github-repo-gallery/js/script.js
+++ github-repo-gallery/js/script.js
@@ -2,14 +2,25 @@
 const profileInfo = document.querySelector("div.overview");
 // select unordered list of repos
 const repoList = document.querySelector("ul.repo-list");
+// select section with class of repos
+const repoSection = document.querySelector("section.repos");
+// select section with individual repo info
+const repoData = document.querySelector("section.repo-data");
 // save github username
 const username = "tiffin-filion";
 
 // fetch github profile
 const fetchGitHubProfile = async function() {
     const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = await response.json();
-    displayInfo(data);
+    if(response.status !== 200) {
+        let div = document.createElement("div");
+        div.classList.add("error");
+        div.innerHTML = response.status + ": " + response.statusText;
+        profileInfo.append(div);
+    } else {
+        const data = await response.json();
+        displayInfo(data);
+    }
 }
 
 // fetch & display user info
@@ -45,6 +56,41 @@ const displayRepos = function(repos) {
         li.append(h3);
         repoList.append(li);
     }
+}
+
+// on click of repo list item
+repoList.addEventListener("click", function(e) {
+    if(e.target.matches("h3")) {
+        let repoName = e.target.innerText;
+        fetchRepoInfo(repoName);
+    }
+})
+
+// fetch repo language info
+const fetchRepoInfo = async function(repoName) {
+    const response = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await response.json();
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+    const languageData = await fetchLanguages.json();
+    const languages = [];
+    for(let l in languageData) {
+        languages.push(l);
+    }
+    displayRepoLanguage(repoInfo, languages);
+}
+
+// display single repo language info
+const displayRepoLanguage = function(repoInfo, languages) {
+    repoData.innerHTML = "";
+    const div = document.createElement("div");
+    div.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    repoData.append(div);
+    repoData.classList.remove("hide");
+    repoList.classList.add("hide");
 }
 
 fetchGitHubProfile();
